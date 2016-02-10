d08554aaef635ebd337f89e542dac7afb5b312e8
Geoff Pitfield <geoff@pitfield.com>
2016-02-09 17:57:20 -0800

    helpers for AWS in go

---
+++ environment.go
@@ -0,0 +1,81 @@
+package aws
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+)
+
+func InstanceID() string {
+	c := http.Client{Timeout: time.Second * 10}
+	resp, err := c.Get("http://169.254.169.254/latest/meta-data/instance-id")
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println(err)
+	}
+	return string(body)
+}
+
+func GetEnvironment() (environment string) {
+	// try to get the variable from the environment
+	environment = os.Getenv("environment")
+	if environment != "" {
+		log.Println("environment from envvar", environment)
+		return
+	}
+	instanceID := InstanceID()
+	if instanceID == "" {
+		environment = "dev"
+		log.Println("environment from default", environment)
+		return
+	}
+	log.Println("instanceID", instanceID)
+	sess := session.New(&aws.Config{Region: aws.String("us-east-1")})
+	svc := ec2.New(sess)
+	params := &ec2.DescribeTagsInput{
+		DryRun: aws.Bool(false),
+		Filters: []*ec2.Filter{
+			{
+				Name: aws.String("key"),
+				Values: []*string{
+					aws.String("environment"),
+				},
+			},
+			{
+				Name: aws.String("resource-id"),
+				Values: []*string{
+					aws.String(instanceID),
+				},
+			},
+			{
+				Name: aws.String("resource-type"),
+				Values: []*string{
+					aws.String("instance"),
+				},
+			},
+		},
+		MaxResults: aws.Int64(10), // NB is error if <5
+	}
+	resp, err := svc.DescribeTags(params)
+	if err != nil {
+		log.Println("describeTags", err.Error())
+		return
+	}
+
+	if len(resp.Tags) == 1 {
+		environment = *resp.Tags[0].Value
+	}
+	log.Println("environment", environment)
+	return
+}
